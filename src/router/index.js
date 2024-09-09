eef5187b3ab1ed9c1a0bec41b08964acd686dafe
Shrijana Karki <srznakrki111@gmail.com>
2024-09-09 18:01:26 +0545

    Updated index.js to implement lazy loading

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from "@/views/AboutView.vue";
-import ProjectView from "@/views/ProjectView.vue";
-import ExperienceView from "@/views/ExperienceView.vue";
-import ContactView from "@/views/ContactView.vue";
+
+const HomeView = () => import('../views/HomeView.vue');
+const AboutView = () => import('@/views/AboutView.vue');
+const ProjectView = () => import('@/views/ProjectView.vue');
+const ExperienceView = () => import('@/views/ExperienceView.vue');
+const ContactView = () => import('@/views/ContactView.vue');
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL), //CreateWebHistory: Enable HTML5 History Mode, for clean URL(without # symbol in the URl)
